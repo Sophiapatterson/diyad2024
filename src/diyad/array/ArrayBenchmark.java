233cd37544d066c1d187b3527de0dd16cec910ad
Owen Astrachan <ola@cs.duke.edu>
2019-01-27 18:28:29 -0500

    small updates

--- src/diyad/array/ArrayBenchmark.java
+++ src/diyad/array/ArrayBenchmark.java
@@ -10,6 +10,9 @@ public class ArrayBenchmark {
 			gsa.add("hello");
 		}
 		double end = System.nanoTime();
+		if (gsa.size() != n) {
+			throw new RuntimeException(String.format("bad size %d expected %d",gsa.size(),n));
+		}
 		return (end-start)/1e9;
 	}
 	
@@ -19,9 +22,10 @@ public class ArrayBenchmark {
 		int end = 1000000;
 		int incr = 100000;
 		
+		System.out.printf("size\ttime\n");
 		for(int size = start; size <= end; size += incr){
 			double time = addStrings(size);
-			System.out.printf("%d,%2.3f\n",size,time);
+			System.out.printf("%10d\t%2.3f\n",size,time);
 		}
 	}
 }
